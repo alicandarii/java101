dcefd0f01796a37408ca576555684d6e94dff15d
alicandari <alicandarii@icloud.com>
2022-03-29 21:31:52 +0300

    YildizlarlaElmas

---
+++ YildizlarlaElmas/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Basamak Sayısı : ");
+        int n = inp.nextInt();
+
+        for (int i = 1;i<=n;i++){
+            for (int k =1;k<=(n-i);k++){
+                System.out.print(" ");
+
+
+            }
+            for (int j=1;j<= (2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+            }
+        for (int i = n;i>1;i--){
+            for (int j =1;j<n-i+2;j++) {
+                System.out.print(" ");
+            }
+                for (int k = 0;k<(i*2)-3;k++){
+                    System.out.print("*");
+                }
+                System.out.println();
+
+        }
+
+
+    }
+}
