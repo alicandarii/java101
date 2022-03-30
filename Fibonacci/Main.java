3b074df7830669c2deb6522d19f81c70e646b22b
alicandari <alicandarii@icloud.com>
2022-03-30 22:29:46 +0300

    Fibonacci

---
+++ Fibonacci/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        int a = 1, b = 1, before = 1, sum = 0;
+        System.out.print(a + " " + b + " ");
+        for (int i = 0; i < 12; i++) {
+            before = a;
+            sum = before + b;
+            a = sum;
+            b = before;
+            System.out.print(sum + " ");
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
