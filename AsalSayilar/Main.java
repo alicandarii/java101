fdcc6ad23551370f1247d8e19e2ceb92cdff7f99
alicandari <alicandarii@icloud.com>
2022-03-30 22:20:21 +0300

    AsalSayilar

---
+++ AsalSayilar/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        int asal = 0, sayac = 0, a = 0;
+        for (int i = 2; i < 100; i++) {
+            for (int j = 2; j < 100; j++)
+                if (i % j == 0)
+                    sayac++;
+            if (sayac < 2)
+                System.out.print(i + " ");
+            sayac = 0;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
