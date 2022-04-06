ce7fbdcbaca6e2192ac04246ccbcc9de7b2d59d5
alicandari <alicandarii@icloud.com>
2022-04-07 00:06:44 +0300

    Palindrom

---
+++ Palindrom/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main {
+
+    static boolean isPalindrom(int num){
+        int temp = num, reverseNum=0,lastNum;
+        while (temp!=0){
+            lastNum = temp %10;
+            reverseNum = (reverseNum*10) + lastNum;
+            temp /=10;
+        }
+        if (num ==reverseNum)
+            return true;
+        else
+            return false;
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(2476));
+    }
+}
